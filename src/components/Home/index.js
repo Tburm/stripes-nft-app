37b03c7865c7a8e27779729c7e0cfed9d2f75aeb
Troy <troy.burmeister@gmail.com>
2021-12-20 14:25:33 -0700

    simple minting app

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 
 // components
 import MintButton from '../MintButton'
+import Gallery from '../Gallery'
 
 function Home(props) {
   if(props.addresses) {
     return (
-      <div className="flex flex-row justify-center w-full">
+      <div className="flex flex-col items-center w-full">
         <div className="card w-3/5">
           <MintButton {...props} />
+        </div>
+        <div className="card w-3/5">
+          <Gallery {...props} />
         </div>
       </div>
     )
   } else {
     return (
-      <div className="flex flex-row justify-center w-full">
+      <div className="flex flex-col items-center w-full">
         <div className="card w-3/5">
-          <p>Connect to network</p>
+          <p>Connect to Metis Stardust</p>
         </div>
       </div>
     )
